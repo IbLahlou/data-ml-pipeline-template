4a6cb6ed7ce556fe990f9014d391d1a98332eaa2
Lahlou ibrahim <ibrahimlahlou021@gmail.com>
2024-06-03 09:17:48 +0000

    MLOPS7 : Adding react vite frontend

--- {{cookiecutter.project_slug}}/bentoml/frontend.py
+++ {{cookiecutter.project_slug}}/bentoml/frontend.py
@@ -9,6 +9,20 @@ MODEL_A_URL = f"{BASE_URL}/model_a_predict"
 MODEL_B_URL = f"{BASE_URL}/model_b_predict"
 AB_TEST_URL = f"{BASE_URL}/predict_ab_test"
 
+{% elif cookiecutter.frontend == "vite-react" %}
+
+import git
+
+# Replace with the repository URL you want to clone
+REPO_URL = 'https://github.com/IbLahlou/ml_api_frontend.git'
+CLONE_DIR = '.'
+
+# Clone the repository
+git.Repo.clone_from(REPO_URL, CLONE_DIR)
+
+print(f'Repository cloned from {REPO_URL} to {CLONE_DIR}')
+
+
 {% endif %}
 
 # Other script template are on going
